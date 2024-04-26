2227a561500d3453bdbd0d598c0342afef4b7495
mebowers5 <mebowers5@gmail.com>
2024-04-26 19:46:56 -0400

    globals update and assigned package for functions

--- R/sporeg-package.R
+++ R/sporeg-package.R
@@ -2,5 +2,9 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom stats aggregate
+#' @importFrom stats na.omit
+#' @importFrom stats sd
+#' @importFrom stats setNames
 ## usethis namespace: end
 NULL
