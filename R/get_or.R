320036c5ba55483364a6d2be7b8290442f744407
mebowers5 <mebowers5@gmail.com>
2024-05-06 19:04:41 -0400

    assign fx package

--- R/get_or.R
+++ R/get_or.R
@@ -6,19 +6,21 @@
 #' @return A data frame object with a point estimate and lower and upper 95% confidence interval values associated with each covariate
 #' @export
 #'
-#' @examples odds_100km <- get_or(fit2.100km) %>%
-#' mutate(res_name = "100km")
+#' @examples
+#' odds_100km <- get_or(fit2.100km) %>%
+#' dplyr::mutate(res_name = "100km")
+
 get_or <- function(model) {
   coefs <- as.data.frame(summary(model)$coefficients)
   row <- row.names(coefs) %in% c('(Intercept)', 'p_a1', 'mean_depth', 'den_rcs', 'd_shore')
   lower <- coefs[row,'Estimate'] - 1.96*coefs[row, 'Std. Error']
   upper <- coefs[row,'Estimate'] + 1.96*coefs[row, 'Std. Error']
-  
+
   edf <- coefs %>%
     tibble::rownames_to_column(var = "Effect") %>%
     dplyr::select(Effect)
-  
+
   or <- cbind(edf, exp(cbind(Odds_Ratio = coefs[row, 'Estimate'], low_CI_95 = lower, hi_CI_95 = upper)))
-  
+
   return(or)
 }
