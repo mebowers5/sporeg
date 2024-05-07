5418f8cba1c613807c77838e61f1dcb95d660a8d
mebowers5 <mebowers5@gmail.com>
2024-05-07 17:30:33 -0400

    make fx internal

--- R/make_line.R
+++ R/make_line.R
@@ -7,12 +7,11 @@
 #' @param end_y latitudinal sf coordinate object from the desired end point
 #'
 #' @return A simple feature geometry object or simple feature linestring object
-#'
-#' @examples
-#' make_line(Longitude, Latitude, lead(Longitude), lead(Latitude))
-#' make_line(sf::st_coordinates(point_geom)[,1], sf::st_coordinates(point_geom)[,2],
-#' lead(sf::st_coordinates(point_geom)[,1]), lead(sf::st_coordinates(point_geom)[,2]))
+#' @keywords internal
+#' @noRd
 #' @export
+
+
 make_line <- function(start_x, start_y, end_x, end_y) {
   sf::st_linestring(matrix(c(start_x, end_x, start_y, end_y), 2, 2))
 }
