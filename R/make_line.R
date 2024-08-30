d4a0890b071e43f7b59d4b0a6c57fad80af645b8
mebowers5 <mebowers5@gmail.com>
2024-08-30 10:52:02 -0400

    remove internal fx status

--- R/make_line.R
+++ R/make_line.R
@@ -10,12 +10,25 @@
 #' @keywords internal
 #' @noRd
 #'
-#' @examples \dontrun{
-#' internal_function()
-#' }
-#'
-
-
+#' @examples
+#' load(system.file("extdata", "at_dly_locs.Rda", package = "sporeg"))
+#' at_lines <- at_dly_locs %>%
+#' group_by(ID, time) %>%
+#' sf::st_transform(3857) %>%
+#' summarise(pt = sf::st_combine(geometry)) %>%
+#' sf::st_centroid() %>%
+#' mutate(lat = sf::st_coordinates(pt)[,2],
+#' lon = sf::st_coordinates(pt)[,1]) %>%
+#' arrange(ID, time) %>%
+#' mutate(start_x = lon, start_y = lat, end_x = lead(lon), end_y = lead(lat)) %>% #Prep data for making lines - ensures proper order
+#' sf::st_as_sf(coords = c("lon", "lat"), crs = 3857) %>%
+#' filter(!is.na(end_y)) %>%
+#' tidyr::nest() %>%
+#' mutate(
+#' data = purrr::map(data,
+#' ~ mutate(.x,
+#' x = purrr::pmap(.l = list(start_x, start_y, end_x, end_y),
+#' .f = make_line))))
 
 make_line <- function(start_x, start_y, end_x, end_y) {
   sf::st_linestring(matrix(c(start_x, end_x, start_y, end_y), 2, 2))
