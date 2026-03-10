93bdacdc6ff01012599063e012c2d09a70877874
Mike O'Brien <mike@obrien.page>
2026-03-10 15:32:58 -0400

    Move data from inst/extdata to data/. Rename all from .Rda to .rda. Remove data.rda.

---
+++ R/data.R
@@ -0,0 +1,59 @@
+#'
+#' @format An sf object with 1341 rows and 4 variables:
+#' \describe{
+#'   \item{ID}{transmitter identifier, a factor with 105 levels}
+#'   \item{time}{POSIXct timestamp of detection, UTC time zone}
+#'   \item{locType}{}
+#'   \item{geometry}{Well-known text of the location of the identified transmitter
+#'     in WGS84/Pseudo-Mercator, EPSG:3857}
+#' }
+"at_dly_locs"
+
+#'
+#' @format An sf object with 62358 rows and 3 variables:
+#' \describe{
+#'   \item{ID}{transmitter identifier, an integer ranging from 1-10}
+#'   \item{time}{POSIXct timestamp of detection, local time zone}
+#'   \item{geometry}{Well-known text of the location of the identified transmitter
+#'     in WGS84/Pseudo-Mercator, EPSG:3857}
+#' }
+"demo_dets"
+
+#'
+"depth_data"
+
+#'
+"fit2.100km"
+
+#'
+"fo_land_barrier"
+
+#'
+"fo_rel_site"
+
+#'
+"fo_stations"
+
+#'
+"fo_sts_pts"
+
+#'
+"fo_study_site"
+
+#'
+"rel_site"
+
+#'
+"res"
+
+#'
+"results"
+
+#'
+"site_depth"
+
+#'
+"study_site"
+
+#'
+"subset"
