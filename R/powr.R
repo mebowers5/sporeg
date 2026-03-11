b720ea702ec927da29e2da0d9a4c44619dd61a5c
Mike O'Brien <mike@obrien.page>
2026-03-11 13:39:32 -0400

    simplify example, remove hard-coded NULL n

--- R/powr.R
+++ R/powr.R
@@ -1,50 +1,38 @@
 #' Run a power analysis
 #'
-#' This is a wrapper function for running a power analysis on iterative simulation and reconstruction methods process.
+#' This is a wrapper function for running a power analysis on iterative simulation
+#'   and reconstruction methods process.
+#'
 #' @param output the resulting data frame from the iterative methods process
 #' @param sig.level numeric. the desired level of significance to achieve
 #' @param power numeric. the desired level of power to achieve
 #' @param delta numeric. the desired effect size to achieve
-#' @param n integer. the number of replicates/sample size. should be assigned `NULL` if desiring sample size
+#' @param n integer. the number of replicates/sample size. should be assigned
+#'   `NULL` if desiring sample size
 #'
 #' @return A data frame object
 #' @export
 #'
 #' @examples
 #' # Use powr wrapper function on example results
-#'
-#' library(sporeg)
-#' library(dplyr)
-#' library(data.table)
-#'
-#' results <- lapply(results, data.table::rbindlist, idcol = 'resolution')
-#' results <- data.table::rbindlist(results, idcol = 'iteration') %>%
-#'   dplyr::left_join(dplyr::tibble(resolution = 1:4,
-#'     res_name = c("100km", "50km", "25km", "10km")),
-#'     by = "resolution")
-#'
-#' df_var <- zero_var(results)
-#' pow_stat <- df_var %>%
-#'   dplyr::group_by(res_name, gid) %>%
-#'   dplyr::summarise(sd = sd(dif)) %>%
-#'   dplyr::ungroup() %>%
-#'   dplyr::group_by(res_name)
-#'
-#' res <- pow_stat %>% dplyr::filter(res_name == "100km")
+#' # Grab 100km resolution
+#' res <- lapply(sporeg::results, `[[`, 1) |>
+#'   dplyr::bind_rows() |>
+#'   # Calculate standard deviation by group
+#'   dplyr::group_by(gid) |>
+#'   dplyr::summarise(sd = sd(dif)) |>
+#'   dplyr::ungroup()
 #'
 #' anims <- 30
 #' power <- 0.8
-#' delta <- anims*0.01 # a delta within 1% of the total number of animals
+#' delta <- anims * 0.01 # a delta within 1% of the total number of animals
 #' sig.level <- 0.95
-#' n <- NULL
 #'
-#' pwr_100km <- powr(res, sig.level, power, delta)
-#' print("Grid cell resolution: 100 km x 100 km")
-#' pwr_100km
+#' powr(res, sig.level, power, delta)
 
-powr <- function(output, sig.level, power, delta, n) {
+powr <- function(output, sig.level, power, delta, n = NULL) {
   stats::power.t.test(
-    n = NULL,
+    n = n,
     sd = max(na.omit(output$sd)),
     sig.level = sig.level,
     power = power,
