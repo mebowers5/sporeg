93db83b12199c0a1298186bb98ab85f93a21155f
Mike O'Brien <mike@obrien.page>
2026-03-11 13:38:30 -0400

    switch from vectorized ifelse to regular if/else; add error if study site is not projected.

--- R/grid_res.R
+++ R/grid_res.R
@@ -1,73 +1,69 @@
 #' Create grid
 #'
 #' This function creates a grid inside a specified polygon.
-#' @param km grid cell resolution in km. one-sided length of grid cell, assumes desired grid cell is to be squared
+#' @param km grid cell resolution in km. one-sided length of grid cell, assumes
+#'   desired grid cell is to be squared
 #' @param study_site simple feature polygon object in which grid cells should be created
 #' @param epsg epsg numeric code for desired coordinate system transformation
-#' @param what `"polygons"` for grid cell polygons or `"centers"` for center points of grid cells
+#' @param what `"polygons"` for grid cell polygons or `"centers"` for center
+#'   points of grid cells
 #'
 #' @return grid comprised of polygons or points depending on `what` parameter
 #' @export
 #'
 #' @examples
-#' # NOTE: The study site must be in a projected coordinate system (e.g., WGS 84; EPSG: 3857)
+#' # NOTE: The study site must be in a projected coordinate system (e.g. EPSG: 3857)
 #' # when it is initially fed into the function grid_res
 #'
-#' library(sporeg)
-#' library(dplyr)
-#' library(sf)
-#'
-#' site_depth <- site_depth %>% sf::st_transform(., 3857)
-#' HS_100km_grid <- grid_res(100, site_depth, 4269, "polygons")
+#' site_depth_3857 <- site_depth |> sf::st_transform(3857)
+#' HS_100km_grid <- grid_res(100, site_depth_3857, 4269, "polygons")
 
 grid_res <- function(km, study_site, epsg, what) {
   grid_spacing <- km * 1000 # Desired kilometers times 1000 m per 1 km
 
-  ifelse(
-    what == "polygons",
-    {
-      grid <- sf::st_make_grid(
-        study_site,
-        square = T,
-        what = what,
-        cellsize = c(grid_spacing, grid_spacing)
-      ) %>% # Create a grid inside the coastal 500 m isobath polygon
-        sf::st_as_sf() %>%
-        dplyr::mutate(gid = seq_along(x)) %>%
-        sf::st_make_valid() %>%
-        sf::st_transform(epsg) %>%
-        sf::st_cast('MULTIPOLYGON')
+  if (sf::st_is_longlat(study_site)) {
+    stop("Study site is not in a projected coordinate system.")
+  }
 
-      grid <- sf::st_intersection(
-        study_site %>% sf::st_transform(epsg),
-        grid
-      ) %>%
-        sf::st_as_sf() %>%
-        dplyr::mutate(gid = seq_along(geometry)) %>%
-        sf::st_make_valid() %>%
-        sf::st_transform(epsg) %>%
-        sf::st_cast('MULTIPOLYGON')
+  if (what == "polygons") {
+    grid <- sf::st_make_grid(
+      study_site,
+      square = T,
+      what = what,
+      cellsize = c(grid_spacing, grid_spacing)
+    ) |> # Create a grid inside the coastal 500 m isobath polygon
+      sf::st_as_sf() |>
+      dplyr::mutate(gid = seq_along(x)) |>
+      sf::st_make_valid() |>
+      sf::st_transform(epsg) |>
+      sf::st_cast('MULTIPOLYGON')
 
-      return(grid)
-    },
-    ifelse(
-      what == "centers",
-      {
-        grid <- sf::st_make_grid(
-          study_site,
-          square = T,
-          what = what,
-          cellsize = c(grid_spacing, grid_spacing)
-        ) %>% # Create a grid inside the coastal 500 m isobath polygon
-          sf::st_as_sf() %>%
-          dplyr::mutate(gid = seq_along(x)) %>%
-          sf::st_make_valid() %>%
-          sf::st_transform(epsg) %>%
-          sf::st_cast('POINT')
+    grid <- sf::st_intersection(
+      study_site |> sf::st_transform(epsg),
+      grid
+    ) |>
+      sf::st_as_sf() |>
+      dplyr::mutate(gid = seq_along(geometry)) |>
+      sf::st_make_valid() |>
+      sf::st_transform(epsg) |>
+      sf::st_cast('MULTIPOLYGON')
 
-        return(grid)
-      },
-      print("Error: Not a valid argument for parameter 'what'")
-    )
-  )
+    return(grid)
+  } else if (what == "centers") {
+    grid <- sf::st_make_grid(
+      study_site,
+      square = T,
+      what = what,
+      cellsize = c(grid_spacing, grid_spacing)
+    ) |> # Create a grid inside the coastal 500 m isobath polygon
+      sf::st_as_sf() |>
+      dplyr::mutate(gid = seq_along(x)) |>
+      sf::st_make_valid() |>
+      sf::st_transform(epsg) |>
+      sf::st_cast('POINT')
+
+    return(grid)
+  } else {
+    stop("Error: Not a valid argument for parameter 'what'")
+  }
 }
