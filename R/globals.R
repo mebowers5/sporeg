60c9b68636ecdda40a80a7b8d7284b82adc24232
Mike O'Brien <mike@obrien.page>
2026-03-12 17:15:05 -0400

    Fix vctrs issue with den_rcvs example

--- R/globals.R
+++ R/globals.R
@@ -42,3 +42,7 @@ utils::globalVariables(
     "tot"
   )
 )
+
+#' Need to have doFuture in Imports due to momentuHMM, but we never actually call it.
+#'  This causes a NOTE in R CMD check
+silence_doFuture_note <- function() doFuture::registerDoFuture()
